778cd47c206f4f93169ea566b13203a561e77f17
Ivana Andric <ivevil@gmail.com>
2022-10-16 22:11:37 +0200

    Adapting post and its style

--- themes/combany/index.php
+++ themes/combany/index.php
@@ -190,25 +190,25 @@
             </div>
             <p><?php the_excerpt(); ?></p>
             <div class="combany__section-news-card-icons">
-              <a href="" class="combany__section-news-card-icon-user">
+            <div class="combany__section-news-card-icon-comment">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
                   <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                   <circle cx="12" cy="7" r="4"></circle>
                 </svg>
-                <p><?= get_the_author_link(); ?></p>
-              </a>
-              <a href="" class="combany__section-news-card-icon-comment">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-circle">
-                  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
-                </svg>
-                <p><?= get_comments_number(get_the_ID()); ?> Comment</p>
-              </a>
-              <a href="" class="combany__section-news-card-icon-likes">
+                <p><?php the_author(); ?></p>
+            </div>
+            <!-- <div class="combany__section-news-card-icon-likes">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-thumbs-up">
                   <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path>
                 </svg>
                 <p>222 Like</p>
-              </a>
+              </div> -->
+              <div class="combany__section-news-card-icon-comment">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-circle">
+                  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
+                </svg>
+                <p><?= get_comments_number(get_the_ID()); ?> Comment</p>
+            </div>
             </div>
           </div>
       <?php endwhile;
